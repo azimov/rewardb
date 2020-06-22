562b6e0def62b24067468f7017f97e8fdf0d7c60
jgilber2 <JGIlber2@its.jnj.com>
2020-06-22 16:46:11 -0700

    removed global.R

--- inst/dashboard/server.R
+++ inst/dashboard/server.R
@@ -5,6 +5,25 @@ library(DT)
 library(rewardb)
 
 server <- function(input, output, session) {
+    # Simple wrapper for always ensuring that database connection is opened and closed
+    # Postgres + DatabaseConnector has problems with connections hanging around
+    queryDb <- function (query, ...) {
+        dbConn <- DatabaseConnector::connect(connectionDetails = appContext$connectionDetails)
+        df <- DatabaseConnector::renderTranslateQuerySql(dbConn, query, schema = appContext$short_name, ...)
+        DatabaseConnector::disconnect(dbConn)
+        return (df)
+    }
+
+    niceColumnName <- list(SOURCE_NAME = "Database", RR = "Relative Risk", C_AT_RISK = "N Unexposed", T_AT_RISK = "N Exposed",
+    C_PT = "Unexposed time (years)", T_PT = "Exposed time (years)", C_CASES = "Unexposed cases", T_CASES = "Exposed cases",
+    LB_95 = "CI95LB", UB_95 = "CI95UB", P_VALUE = "P", I2 = "I-square")
+
+    niceColumnNameInv <- list()
+
+    for (n in names(niceColumnName)) {
+        niceColumnNameInv[niceColumnName[[n]]] <- n
+    }
+
 
     # Query full results, only filter is Risk range parameters
     mainTableRe <- reactive({
