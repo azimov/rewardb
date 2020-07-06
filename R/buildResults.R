d470be878983e0fd90ce85008879ba4c80093ee7
jgilber2 <JGIlber2@its.jnj.com>
2020-07-06 11:18:45 -0700

    started config

---
+++ R/buildResults.R
@@ -0,0 +1,17 @@
+execute <- function () {
+    # createReferenceTables
+
+    # createCohorts
+
+    # createOutcomeCohorts
+
+    # createOutcomeSummary
+
+    # run SCC
+
+    # addCustomSccOutcome
+}
+
+# Combine results
+
+# compile results
