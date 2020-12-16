6daa9eb14ab3a852f339dd4385bd54ea8a121004
jgilber2 <JGIlber2@its.jnj.com>
2020-12-16 15:10:42 -0800

    Redshift copy utility

--- R/ReferenceIO.R
+++ R/ReferenceIO.R
@@ -21,32 +21,32 @@ exportReferenceTables <- function(
 ) {
   connection <- DatabaseConnector::connect(connectionDetails = config$connectionDetails)
   tryCatch(
-    {
-      # Collect all files and make a hash
-      meta <- list()
-      meta$hashList <- list()
-      meta$tableNames <- CONST_REFERENCE_TABLES
+  {
+    # Collect all files and make a hash
+    meta <- list()
+    meta$hashList <- list()
+    meta$tableNames <- CONST_REFERENCE_TABLES
 
-      for (table in rewardb::CONST_REFERENCE_TABLES) {
-        data <- DatabaseConnector::renderTranslateQuerySql(
-          connection,
-          "SELECT * FROM @schema.@table;",
-          schema = config$rewardbResultsSchema,
-          table = table
-        )
-        file <- file.path(exportPath, paste0(table, ".csv"))
-        write.csv(data, file, na = "", row.names = FALSE)
-        meta$hashList[[basename(file)]] <- tools::md5sum(file)[[1]]
-      }
+    for (table in rewardb::CONST_REFERENCE_TABLES) {
+      data <- DatabaseConnector::renderTranslateQuerySql(
+        connection,
+        "SELECT * FROM @schema.@table;",
+        schema = config$rewardbResultsSchema,
+        table = table
+      )
+      file <- file.path(exportPath, paste0(table, ".csv"))
+      write.csv(data, file, na = "", row.names = FALSE)
+      meta$hashList[[basename(file)]] <- tools::md5sum(file)[[1]]
+    }
 
-      metaDataFilename <- file.path(exportPath, rewardb::CONST_META_FILE_NAME)
-      jsonlite::write_json(meta, metaDataFilename)
+    metaDataFilename <- file.path(exportPath, rewardb::CONST_META_FILE_NAME)
+    jsonlite::write_json(meta, metaDataFilename)
 
-      exportFiles <- file.path(exportPath, paste0(rewardb::CONST_REFERENCE_TABLES, ".csv"))
-      zip::zipr(exportZipFile, append(exportFiles, metaDataFilename), include_directories = FALSE)
+    exportFiles <- file.path(exportPath, paste0(rewardb::CONST_REFERENCE_TABLES, ".csv"))
+    zip::zipr(exportZipFile, append(exportFiles, metaDataFilename), include_directories = FALSE)
 
-      ParallelLogger::logInfo(paste("Created export zipfile", exportZipFile))
-    },
+    ParallelLogger::logInfo(paste("Created export zipfile", exportZipFile))
+  },
     error = ParallelLogger::logError
   )
   DatabaseConnector::disconnect(connection)
@@ -60,46 +60,49 @@ importReferenceTables <- function(cdmConfig, zipFilePath, refFolder, usePgCopy =
   connection <- DatabaseConnector::connect(connectionDetails = cdmConfig$connectionDetails)
 
   tryCatch(
-    {
-      sql <- SqlRender::readSql(system.file("sql/create", "referenceTables.sql", package = "rewardb"))
-      DatabaseConnector::renderTranslateExecuteSql(
-        connection,
-        sql,
-        schema = cdmConfig$referenceSchema,
-        concept_set_definition = cdmConfig$tables$conceptSetDefinition,
-        cohort_definition = cdmConfig$tables$cohortDefinition,
-        outcome_cohort_definition = cdmConfig$tables$outcomeCohortDefinition,
-        atlas_outcome_reference = cdmConfig$tables$atlasOutcomeReference,
-        atlas_concept_reference = cdmConfig$tables$atlasConceptReference,
-        custom_exposure = cdmConfig$tables$customExposure,
-        custom_exposure_concept = cdmConfig$tables$customExposureConcept,
-        analysis_setting = cdmConfig$tables$analysisSetting
-      )
+  {
+    sql <- SqlRender::readSql(system.file("sql/create", "referenceTables.sql", package = "rewardb"))
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection,
+      sql,
+      schema = cdmConfig$referenceSchema,
+      concept_set_definition = cdmConfig$tables$conceptSetDefinition,
+      cohort_definition = cdmConfig$tables$cohortDefinition,
+      outcome_cohort_definition = cdmConfig$tables$outcomeCohortDefinition,
+      atlas_outcome_reference = cdmConfig$tables$atlasOutcomeReference,
+      atlas_concept_reference = cdmConfig$tables$atlasConceptReference,
+      custom_exposure = cdmConfig$tables$customExposure,
+      custom_exposure_concept = cdmConfig$tables$customExposureConcept,
+      analysis_setting = cdmConfig$tables$analysisSetting
+    )
 
-      fileList <- file.path(refFolder, paste0(rewardb::CONST_REFERENCE_TABLES, ".csv"))
-      for (file in fileList) {
-        snakeName <- SqlRender::snakeCaseToCamelCase(strsplit(basename(file), ".csv")[[1]])
-        tableName <- cdmConfig$tables[[snakeName]]
+    fileList <- file.path(refFolder, paste0(rewardb::CONST_REFERENCE_TABLES, ".csv"))
+    for (file in fileList) {
+      snakeName <- SqlRender::snakeCaseToCamelCase(strsplit(basename(file), ".csv")[[1]])
+      tableName <- cdmConfig$tables[[snakeName]]
 
-        if (cdmConfig$connectionDetails$dbms == "postgresql" & usePgCopy) {
-          print(paste("Using pgcopy to upload", snakeName, tableName, file))
-          pgCopy(connectionDetails = cdmConfig$connectionDetails, csvFileName = file, schema = cdmConfig$referenceSchema, tableName = tableName)
-        } else {
-          print(paste("Using insert table", snakeName, tableName, file))
-          data <- read.csv(file)
-          DatabaseConnector::insertTable(
-            connection = connection,
-            tableName = paste(cdmConfig$referenceSchema, tableName, sep = "."),
-            data = data,
-            progressBar = TRUE,
-            dropTableIfExists = TRUE,
-            useMppBulkLoad = cdmConfig$useMppBulkLoad,
-            oracleTempSchema = cdmConfig$oracleTempSchema
-          )
-        }
+      if (cdmConfig$connectionDetails$dbms == "postgresql" & usePgCopy) {
+        print(paste("Using pgcopy to upload", snakeName, tableName, file))
+        pgCopy(connectionDetails = cdmConfig$connectionDetails, csvFileName = file, schema = cdmConfig$referenceSchema, tableName = tableName)
       }
+      else if (cdmConfig$connectionDetails$dbms == "redshift" & cdmConfig$useMppBulkLoad) {
 
-    },
+      } else {
+        print(paste("Using insert table", snakeName, tableName, file))
+        data <- read.csv(file)
+        DatabaseConnector::insertTable(
+          connection = connection,
+          tableName = paste(cdmConfig$referenceSchema, tableName, sep = "."),
+          data = data,
+          progressBar = TRUE,
+          dropTableIfExists = TRUE,
+          useMppBulkLoad = cdmConfig$useMppBulkLoad,
+          oracleTempSchema = cdmConfig$oracleTempSchema
+        )
+      }
+    }
+
+  },
 
     error = function(err) {
       ParallelLogger::logError(err)
