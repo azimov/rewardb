6afb102285bed3d04755ed676849e19b01703529
jgilber2 <JGIlber2@its.jnj.com>
2021-04-15 13:10:45 -0700

    Dashboard creator and neuroscience add hoc cohorts run

--- scripts/adHocAtlasCohorts.R
+++ scripts/adHocAtlasCohorts.R
@@ -1,8 +1,10 @@
 devtools::load_all()
-atlasIds <- c(7542, 7551, 7552, 7553, 7576, 7543, 7545, 7546, 7507, 7547, 7548, 7549, 7550, 7822,
-             7823, 10357, 11073, 2538, 10605, 15078, 10607, 11643, 12047, 14615, 13719, 10977, 19398)
+#atlasIds <- c(7542, 7551, 7552, 7553, 7576, 7543, 7545, 7546, 7507, 7547, 7548, 7549, 7550, 7822,
+             #7823, 10357, 11073, 2538, 10605, 15078, 10607, 11643, 12047, 14615, 13719, 10977, 19398)
 
-refZipFile <- "atlas-pre-existing-reward-reference.zip"
+atlasIds <- c(20024, 20042, 20001, 17619, 10977, 20655, 19511, 19514, 19550, 19802, 19804, 20004, 20364, 20421)
+
+refZipFile <- "ns-reward-reference.zip"
 config <- loadGlobalConfig("config/global-cfg.yml")
 
 configId <- paste("atlasRun-pre-existing")
@@ -10,7 +12,8 @@ cdmConfigPaths <-c(
   "config/cdm/mcdc.yml",
   "config/cdm/mcdr.yml",
   "config/cdm/ccae.yml",
-  "config/cdm/optum.yml"
+  "config/cdm/optum.yml",
+  "config/cdm/jdmc.yml",
 )
 
 
@@ -26,7 +29,7 @@ for (cdmConfigPath in cdmConfigPaths) {
 
 
 ## Exposure cohorts
-atlasExposureId <- c(19177, 19178)
+atlasExposureId <- c()
 resultsFiles <- c()
 for (cdmConfigPath in cdmConfigPaths) {
   resultsFiles <- sccAdHocCohorts(cdmConfigPath, configId, atlasExposureId, sourceUrl = config$webApiUrl, exposure = TRUE)
