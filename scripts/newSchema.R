a5f13e1f3afecd8cae90aa172962a4bb9388a21e
jgilber2 <JGIlber2@its.jnj.com>
2021-07-20 17:08:22 -0700

    migration script and updated cohort ids

--- scripts/newSchema.R
+++ scripts/newSchema.R
@@ -5,8 +5,9 @@ connection <- DatabaseConnector::connect(connectionDetails = config$connectionDe
 # Set up a database schema
 buildPgDatabase("config/global-cfg.yml")
 
-atlasCohorts <- c(7542, 7551, 7552, 7553, 7576, 7543, 7545, 7546, 7507, 7547, 7548, 7549, 7550, 7822,
-                  7823, 10357, 11073, 2538, 10605, 15078, 10607, 11643, 12047, 14615, 13719, 10977, 19398)
+atlasCohorts <- c(381, 382, 1257, 1258, 1259, 1260, 1261, 1262, 1263, 1264, 1265, 1267, 1268, 1269, 1270, 1271, 1272,
+                  1273, 1274, 1275, 1276, 1277, 1278, 1279, 1280, 1281, 1282, 1283, 1284, 1286, 1287, 1288, 1289, 1290,
+                  1291, 1292, 1293, 1294, 1295, 1296)
 
 connection <- DatabaseConnector::connect(connectionDetails = config$connectionDetails)
 # Add atlas cohorts
