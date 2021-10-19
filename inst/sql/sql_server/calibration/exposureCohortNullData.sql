ff4fe1735b2912acd46cac8e4611800d0ff44b13
jgilber2 <JGIlber2@its.jnj.com>
2021-10-19 15:12:49 -0700

    Misnamed files

--- inst/sql/sql_server/calibration/exposureCohortNullData.sql
+++ inst/sql/sql_server/calibration/exposureCohortNullData.sql
@@ -1,11 +1,12 @@
-IF OBJECT_ID('@results_schema.exposure_cohort_null_data', 'U') IS NOT NULL
-    DROP TABLE @results_schema.exposure_cohort_null_data;
+IF OBJECT_ID('@results_schema.outcome_cohort_null_data', 'U') IS NOT NULL
+    DROP TABLE @results_schema.outcome_cohort_null_data;
 
-CREATE TABLE @results_schema.exposure_cohort_null_data AS (
-    SELECT sr.*
-    FROM @results_schema.outcome_negative_control_concept oncc
-    INNER JOIN @results_schema.cohort_definition cd ON (cd.drug_conceptset_id = oncc.concept_id)
-    INNER JOIN @results_schema.scc_result sr ON (sr.target_cohort_id = cd.cohort_definition_id AND sr.outcome_cohort_id = oncc.cohort_definition_id)
+CREATE TABLE @results_schema.outcome_cohort_null_data AS (
+    SELECT sr.*, ocd.outcome_type
+    FROM @results_schema.exposure_negative_control_concept encc
+    INNER JOIN @results_schema.cohort_definition cd on encc.cohort_definition_id = cd.cohort_definition_id
+    INNER JOIN @results_schema.outcome_cohort_definition ocd ON (ocd.conceptset_id = encc.concept_id)
+    INNER JOIN @results_schema.scc_result sr ON (sr.outcome_cohort_id = ocd.cohort_definition_id AND sr.target_cohort_id = encc.cohort_definition_id)
     -- Join CEM ingredients to reward cohorts
     WHERE sr.analysis_id IN (@analysis_id)
     AND sr.rr IS NOT NULL
